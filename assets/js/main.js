0f65f2fef3443df98a7368cbb2a3a19ac6e31677
giorgiadifranco <giorgiadf08@gmail.com>
2024-10-11 14:42:42 +0200

    add array

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,30 @@
 console.log('ciao');
 
+const bici = [
+    {
+        nome: 'triban',
+        peso: 11.25
+
+},
+{
+    nome: 'Van Rysel EDR 920 CF ULTEGRA ',
+    peso: 7.28
+
+},    {
+    nome: 'VAN RYSEL NCR CF SHIMANO TIAGRA',
+    peso: 9
+
+},    {
+    nome: 'NCR AF',
+    peso: 10
+
+},    {
+    nome: 'Triban RC 100',
+    peso: 11.3
+
+},    {
+    nome: 'RCR RIVAL AXS ROUTE ZIPP 303',
+    peso: 8.5
+
+}
+]
